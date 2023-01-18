defa82e6e6da69220290bf8835351a0311a1f8b4
Ubica228 <122717010+Ubica228@users.noreply.github.com>
2023-01-19 04:36:07 +0500

    Update js.js

--- js.js
+++ js.js
@@ -2,7 +2,7 @@ let tg = window.Telegram.WebApp;
 tg.expand();
 
 phone = document.getElementById('phone_code') ;
-
+send_btn = document.getElementById('send-button');
 send_btn.addEventListener('click',function(){
  
     tg.sendData(phone.value);
